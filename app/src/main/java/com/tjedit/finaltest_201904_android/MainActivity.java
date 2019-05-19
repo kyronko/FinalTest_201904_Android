ebb3ebfd154cb60fa286d606759250a0d9226338
user <kyronko@naver.com>
2019-05-19 11:14:50 +0900

    데이터바인딩과 베이스액티비티제작

---
+++ app/src/main/java/com/tjedit/finaltest_201904_android/MainActivity.java
@@ -0,0 +1,34 @@
+package com.tjedit.finaltest_201904_android;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tjedit.finaltest_201904_android.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         bindViews();
+         setupEvents();
+         setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+}
