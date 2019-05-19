0147a81568f37a62e3d1ebefa12b660c04bc5a60
user <kyronko@naver.com>
2019-05-19 13:58:46 +0900

    메인액티비티 생성

--- app/src/main/java/com/tjedit/finaltest_201904_android/MainActivity.java
+++ app/src/main/java/com/tjedit/finaltest_201904_android/MainActivity.java
@@ -1,13 +1,31 @@
 package com.tjedit.finaltest_201904_android;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tjedit.finaltest_201904_android.databinding.ActivityLoginBinding;
 
+public class MainActivity extends  BaseActivity{
+ActivityLoginBinding act;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 }
